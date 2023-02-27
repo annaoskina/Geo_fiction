5b32dde418f7b3cfa8b2d19026feef76bbaca1d6
Anna Oskina <annaoskina@gmail.com>
2023-02-27 14:53:27 +0600

    new files from code

--- coordinates.py
+++ coordinates.py
@@ -1,9 +1,9 @@
 from geopy.geocoders import Nominatim
 import csv
 
-def extract_tsv(name_tsv):
-	with open(name_tsv, encoding = 'utf-8') as r_file:
-		data = csv.reader(r_file, delimiter = '\t')
+def extract_csv(name_csv):
+	with open(name_csv, encoding = 'utf-8') as r_file:
+		data = csv.reader(r_file, delimiter = ',')
 		data_list = list(data)
 	return data_list
 
@@ -13,34 +13,29 @@ def find_coordinates(some_place):
 	lat = location.latitude
 	lon = location.longitude
 	coordinates = []
-	coordinates.append(lat)
-	coordinates.append(lon)
+	coordinates.append(str(lat))
+	coordinates.append(str(lon))
 	#print(coordinates)
 	return coordinates
 
 def write_csv(data_list, filename):
-	with open('{}.tsv'.format(filename), 'w', encoding = 'utf-8') as file:
+	with open('{}.csv'.format(filename), 'w', encoding = 'utf-8') as file:
 	    for row in data_list:
-	    	file.write(row)
+	    	row.append('\n')
+	    	for srting in row:
+	    		file.write(string)
 	return 0
 
-name_tsv = 'clean_result.tsv'
-data_new_list = extract_tsv(name_tsv)
+name_csv = 'result_tally.csv'
+data_new_list = extract_csv(name_csv)
 for line in data_new_list:
-	if line[5]:
-		place_name = line[5]
+	if line[3]:
+		place_name = line[3]
 		try:
 			line.extend(find_coordinates(place_name))
 		except:
 			print(place_name)
-write_csv(data_new_list, coordinates)
+
+filename = 'places_with_coordinates'
+write_csv(data_new_list, filename)
 print('Done')
-
-
-
-
-
-#		lon_lat = find_coordinates(row[5])
-#		row = row + '\t' + coordinates
-#	print(type(row))
-#write_csv(data_new_list, data_with_coordinates)
